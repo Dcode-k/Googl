c338df41e389ce5f454d616c6c4383a27ce3b8ff
Krishnkant <krishnkantpachori50@gmail.com>
2021-11-21 10:10:53 +0530

    okk

--- src/contexts/StateContextProvider.js
+++ src/contexts/StateContextProvider.js
@@ -1,37 +1,44 @@
+/* eslint-disable no-shadow */
 /* eslint-disable no-unused-vars */
-import React, { createContext, useContext, useState } from 'react';
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable quotes */
+import React, { createContext, useContext, useState } from "react";
 
 const StateContext = createContext();
-const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
 
 export const StateContextProvider = ({ children }) => {
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
   const getResults = async (url) => {
     setLoading(true);
 
     const res = await fetch(`${baseUrl}${url}`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'x-user-agent': 'desktop',
-        'x-proxy-location': 'US',
-        'x-rapidapi-host': 'google-search3.p.rapidapi.com',
-        'x-rapidapi-key': process.env.REACT_APP_API_KEY,
+        
+        "x-user-agent": "desktop",
+        "x-proxy-location": "US",
+        "x-rapidapi-host": "google-search3.p.rapidapi.com",
+        "x-rapidapi-key": process.env.REACT_APP_API_KEY,
       },
     })
-      // eslint-disable-next-line no-shadow
       .then((res) => res.json())
       .then((data) => {
-        setResults(data.results);
+        setResults(data);
         setLoading(false);
       })
-      .catch((err) => res.status(501).send('User- query promise was rejected. Handle according to specific case.'));
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
-    <StateContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, loading }}>
+    <StateContext.Provider
+      value={{ getResults, results, searchTerm, setSearchTerm, loading }}
+    >
       {children}
     </StateContext.Provider>
   );
